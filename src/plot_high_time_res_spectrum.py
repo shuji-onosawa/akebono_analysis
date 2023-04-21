3f55795969f3bf71d49eb8deeb74f31d2d7dd3e8
shuji-onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2023-04-22 02:50:34 +0900

    update requirements.txt add program to interpolate an epoch array from mgf cdf file. add program to plot high time resolution dynamic spectrum of mca with the variation of the angle between B0 and anntenas.

--- src/plot_high_time_res_spectrum.py
+++ src/plot_high_time_res_spectrum.py
@@ -1,10 +1,9 @@
 import pytplot
 import numpy as np
 from get_antenna_angle import unit_vector, angle_between_vectors
+from interpolate_mgf_epoch import interpolate_mgf_epoch
 import cdflib
-from load import mca_h1cdf_dB_to_absolute
-import matplotlib.pyplot as plt
-import datetime as dt
+import akebono
 
 date = '19900225'
 trange = [date[0:4]+'-'+date[4:6]+'-'+date[6:8]+' 12:20:00',
@@ -14,19 +13,19 @@ Ey_antenna_vector = np.array([-np.sin(np.deg2rad(35)),
                               0])
 sBy_antenna_vector = np.array([0.0, -1.0, 0.0])
 
-cdf_name = './akebono_data/mgf/ak_h0_mgf_'+date+'_v01.cdf'
+cdf_name = '../akebono_data/mgf/ak_h0_mgf_'+date+'_v01.cdf'
 mgf_xary = cdflib.cdf_to_xarray(cdf_name)
 
-B0_epoch = mgf_xary['Epoch'].data / 1000
+B0_epoch = mgf_xary['Epoch'].data
 B0_ary = mgf_xary['B0_spin'].data
 
-angle_btwn_B0_Ey = np.empty(int((B0_ary.shape[0]+1)/2-1))
-angle_btwn_B0_sBy = np.empty(int((B0_ary.shape[0]+1)/2-1))
-angle_btwn_B0_antenna_epoch = np.empty(int((B0_ary.shape[0]+1)/2-1))
+angle_btwn_B0_Ey = np.empty(int(B0_ary.shape[0]))
+angle_btwn_B0_sBy = np.empty(int(B0_ary.shape[0]))
+angle_btwn_B0_antenna_epoch = np.empty(int(B0_ary.shape[0]))
 
-for i in range(int((B0_ary.shape[0]+1)/2-1)):
-    # 1秒値データを想定しています。
-    if B0_epoch[2*i] == 0.0:
+for i in range(int(B0_ary.shape[0])):
+
+    if B0_epoch[i] == 0.0:
         angle_btwn_B0_antenna_epoch[i] = 0.0
         angle_btwn_B0_Ey[i] = np.nan
         angle_btwn_B0_sBy[i] = np.nan
@@ -34,32 +33,33 @@ for i in range(int((B0_ary.shape[0]+1)/2-1)):
         # mgfのデータ構造
         # epoch: 長さ172799の1次元配列 (1日/0.5秒=172800)
         # b0_spin: 172799*3*16の3次元配列
-        # 1秒値にするために2つの連続する3*16の配列の最後と最初の値をとってきて平均
-        angle_btwn_B0_antenna_epoch[i] = B0_epoch[2*i]
-        B0_vector = np.array([(B0_ary[2*i][0][15]+B0_ary[2*i+1][0][0])/2,
-                              (B0_ary[2*i][1][15]+B0_ary[2*i+1][1][0])/2,
-                              (B0_ary[2*i][2][15]+B0_ary[2*i+1][2][0])/2])
+        # 0.5sごとに3軸の16個のデータがあり、各軸の平均値を取る
+        angle_btwn_B0_antenna_epoch[i] = B0_epoch[i]
+        B0_vector = np.array([np.average(B0_ary[i][0]),
+                              np.average(B0_ary[i][1]),
+                              np.average(B0_ary[i][2])])
 
         B0_unit_vector = unit_vector(B0_vector)
         angle_btwn_B0_Ey[i] = angle_between_vectors(B0_unit_vector, Ey_antenna_vector)
         angle_btwn_B0_sBy[i] = angle_between_vectors(B0_unit_vector, sBy_antenna_vector)
 
+mgf_epoch_interp = interpolate_mgf_epoch(angle_btwn_B0_antenna_epoch)
+
 pytplot.store_data(name='angle_btwn_B0_Ey',
-                   data={'x': angle_btwn_B0_antenna_epoch,
+                   data={'x': cdflib.cdfepoch.to_datetime(mgf_epoch_interp),
                          'y': angle_btwn_B0_Ey})
 pytplot.options(name='angle_btwn_B0_Ey',
                 opt_dict={'ytitle': 'angle between B0 and E1',
                           'yrange': [-180, 180]})
 pytplot.store_data(name='angle_btwn_B0_sBy',
-                   data={'x': angle_btwn_B0_antenna_epoch,
+                   data={'x': cdflib.cdfepoch.to_datetime(mgf_epoch_interp),
                          'y': angle_btwn_B0_sBy})
 pytplot.options(name='angle_btwn_B0_sBy',
                 opt_dict={'ytitle': 'angle between B0 and B1',
                           'yrange': [-180, 180]})
 
+akebono.vlf_mca(trange=['1990-2-25', '1990-2-26'], datatype='pwr')
 
-pytplot.cdf_to_tplot('./akebono_data/vlf/mca/h1/ave8s/ak_h1_mca_'+date+'_v02.cdf')
-mca_h1cdf_dB_to_absolute(spec_type='pwr')
-Emax_tvar = pytplot.get_data('Emax')
 pytplot.tlimit(trange)
-pytplot.tplot(['Emax_pwr', 'Bmax_pwr'], save_png='./plots/mca_spin_modulation/mca_spec_ave8s_wide')
+pytplot.tplot(['akb_mca_Emax_pwr', 'angle_btwn_B0_Ey', 'akb_mca_Bmax_pwr', 'angle_btwn_B0_sBy'],
+              save_png='../plots/mca_spin_modulation/mca_spec_ave0.5s_wide')
