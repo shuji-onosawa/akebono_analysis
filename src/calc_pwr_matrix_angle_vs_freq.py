77b62057a5be659a9c989b3c053be7b4041e0fe2
onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2023-06-07 15:47:22 +0900

    Refactor code and update dependencies

--- src/calc_pwr_matrix_angle_vs_freq.py
+++ src/calc_pwr_matrix_angle_vs_freq.py
@@ -14,14 +14,16 @@ def make_wave_mgf_dataset(date: str,
     mgf_epoch = mgf_with_angle_dataset['Epoch'].values
 
     # MCAのデータをtplot変数にする
-    store_mca_high_time_res_data(date=date, datatype=mca_datatype)
+    store_mca_high_time_res_data(date=date,
+                                 datatype=mca_datatype,
+                                 del_invalid_data=['off', 'bit rate m', 'sms', 'bdr', 'noisy'])
     # MCAのデータを取り出す
     mca_Emax_tvar = pytplot.get_data('akb_mca_Emax_'+mca_datatype)
     mca_Bmax_tvar = pytplot.get_data('akb_mca_Bmax_'+mca_datatype)
     mca_epoch = mca_Emax_tvar.times
     mca_Emax_data = mca_Emax_tvar.y
     mca_Bmax_data = mca_Bmax_tvar.y
-    
+
     if len(mca_epoch) == len(mgf_epoch):
         pass
     # MGFのepoch(ndarray)の長さにMCAのepoch(ndarray)の長さを合わせる
@@ -47,70 +49,28 @@ def make_wave_mgf_dataset(date: str,
     return wave_mgf_dataset
 
 
-def calc_pwr_matrix_angle_vs_freq(date: str = '1990-02-11',
-                                  start_time: str = '18:05:10',
-                                  end_time: str = '18:09:00',
-                                  mca_datatype: str = 'pwr'):
-    wave_mgf_dataset = make_wave_mgf_dataset(date=date, mca_datatype=mca_datatype)
-    # datasetからstart_time_epochとend_time_epochの間のsubsetを作成する
-    start_time_epoch = np.datetime64(date+'T'+start_time)
-    end_time_epoch = np.datetime64(date+'T'+end_time)
-    sub_wave_mgf_dataset = wave_mgf_dataset.sel(Epoch=slice(start_time_epoch, end_time_epoch))
-
-    return sub_wave_mgf_dataset
-
-    '''    mca_Emax_pwr = mca_Emax_pwr.y[idx_start_time_epoch:idx_end_time_epoch]
-    mca_Bmax_pwr = mca_Bmax_pwr.y[idx_start_time_epoch:idx_end_time_epoch]
-    mgf_B0_Ey = mgf_B0_Ey.y[idx_start_time_epoch:idx_end_time_epoch]
-    mgf_B0_sBy = mgf_B0_sBy.y[idx_start_time_epoch:idx_end_time_epoch]
-
-    # 8個おきにデータを
-    new_mca_Emax_pwr = np.zeros((1, 16))
-    new_mca_Bmax_pwr = np.zeros((1, 16))
-    new_angle_ary_Ey = np.array([])
-    new_angle_ary_sBy = np.array([])
-    for i in range(8):
-        mca_Emax_pwr_same_angle = mca_Emax_pwr[i::8]
-        mca_Bmax_pwr_same_angle = mca_Bmax_pwr[i::8]
-        mgf_B0_Ey_same_angle = mgf_B0_Ey[i::8]
-        mgf_B0_sBy_same_angle = mgf_B0_sBy[i::8]
-        # さらに2個ずつのデータを取得する
-        mgf_B0_Ey_same_angle = mgf_B0_Ey_same_angle[::2]
-        mgf_B0_sBy_same_angle = mgf_B0_sBy_same_angle[::2]
-        new_mca_Emax_pwr_same_angle_0 = mca_Emax_pwr_same_angle[::2]
-        new_mca_Emax_pwr_same_angle_1 = mca_Emax_pwr_same_angle[1::2]
-        new_mca_Bmax_pwr_same_angle_0 = mca_Bmax_pwr_same_angle[::2]
-        new_mca_Bmax_pwr_same_angle_1 = mca_Bmax_pwr_same_angle[1::2]
-        if len(new_mca_Emax_pwr_same_angle_0) != len(new_mca_Emax_pwr_same_angle_1):
-            new_mca_Emax_pwr_same_angle_0 = new_mca_Emax_pwr_same_angle_0[:-1]
-            mgf_B0_Ey_same_angle = mgf_B0_Ey_same_angle[:-1]
-            mgf_B0_sBy_same_angle = mgf_B0_sBy_same_angle[:-1]
-        new_mca_Emax_pwr_same_angle = (new_mca_Emax_pwr_same_angle_0 + new_mca_Emax_pwr_same_angle_1)/2
-        new_mca_Bmax_pwr_same_angle = (new_mca_Bmax_pwr_same_angle_0 + new_mca_Bmax_pwr_same_angle_1)/2
-        new_mca_Emax_pwr = np.append(new_mca_Emax_pwr, new_mca_Emax_pwr_same_angle, axis=0)
-        new_mca_Bmax_pwr = np.append(new_mca_Bmax_pwr, new_mca_Bmax_pwr_same_angle, axis=0)
-        new_angle_ary_Ey = np.append(new_angle_ary_Ey, mgf_B0_Ey_same_angle)
-        new_angle_ary_sBy = np.append(new_angle_ary_sBy, mgf_B0_sBy_same_angle)
-    new_mca_Emax_pwr = new_mca_Emax_pwr[1:]
-    new_mca_Bmax_pwr = new_mca_Bmax_pwr[1:]
-
-    # mgf_B0_Eyが0-22.5度, 22.5-45度, 45-67.5度, 67.5-90度, 90-112.5度, 112.5-135度, 135-157.5度, 157.5-180度の時のMCAのデータ(16*1の配列)を取得したものを平均して、角度ごとのMCAのデータ(8*16の配列)を作成する
-    e_matrix = [get_data_in_angle_range(mgf_B0_Ey, new_mca_Emax_pwr, [0, 22.5]),
-                get_data_in_angle_range(mgf_B0_Ey, new_mca_Emax_pwr, [22.5, 45]),
-                get_data_in_angle_range(mgf_B0_Ey, new_mca_Emax_pwr, [45, 67.5]),
-                get_data_in_angle_range(mgf_B0_Ey, new_mca_Emax_pwr, [67.5, 90]),
-                get_data_in_angle_range(mgf_B0_Ey, new_mca_Emax_pwr, [90, 112.5]),
-                get_data_in_angle_range(mgf_B0_Ey, new_mca_Emax_pwr, [112.5, 135]),
-                get_data_in_angle_range(mgf_B0_Ey, new_mca_Emax_pwr, [135, 157.5]),
-                get_data_in_angle_range(mgf_B0_Ey, new_mca_Emax_pwr, [157.5, 180])]
-    m_matrix = [get_data_in_angle_range(mgf_B0_sBy, new_mca_Bmax_pwr, [0, 22.5]),
-                get_data_in_angle_range(mgf_B0_sBy, new_mca_Bmax_pwr, [22.5, 45]),
-                get_data_in_angle_range(mgf_B0_sBy, new_mca_Bmax_pwr, [45, 67.5]),
-                get_data_in_angle_range(mgf_B0_sBy, new_mca_Bmax_pwr, [67.5, 90]),
-                get_data_in_angle_range(mgf_B0_sBy, new_mca_Bmax_pwr, [90, 112.5]),
-                get_data_in_angle_range(mgf_B0_sBy, new_mca_Bmax_pwr, [112.5, 135]),
-                get_data_in_angle_range(mgf_B0_sBy, new_mca_Bmax_pwr, [135, 157.5]),
-                get_data_in_angle_range(mgf_B0_sBy, new_mca_Bmax_pwr, [157.5, 180])]
-
-    return e_matrix, m_matrix
+def calc_pwr_matrix_angle_vs_freq(dataset: xr.Dataset,
+                                  data_name: str,
+                                  angle_name: str):
     '''
+    Parameters
+    ----------
+    date : str, optional
+        日付, yyyy-mm-dd
+    data_name : str, optional
+        例: 'akb_mca_Emax_pwr'
+    angle_name : str, optional
+        例: 'angle_b0_Ey'
+    '''
+    angle_list = [-180, -157.5, -135, -112.5, -90, -67.5, -45, -22.5, 0,
+                  22.5, 45, 67.5, 90, 112.5, 135, 157.5, 180]
+    mean_pwr_matrix = np.zeros((len(angle_list)-1, 16))
+    for i in range(len(angle_list)-1):
+        condition = (dataset[angle_name] >= angle_list[i]) &\
+                    (dataset[angle_name] < angle_list[i+1])
+        dataset_in_angle_range = dataset.where(condition, drop=True)
+        if dataset_in_angle_range[angle_name].size == 0:
+            mean_pwr_matrix[i, :] = np.nan
+        else:
+            mean_pwr_matrix[i] = dataset_in_angle_range[data_name].mean(dim='Epoch')
+    return mean_pwr_matrix
--- src/store_high_time_res_spectrum_data.py
+++ src/store_high_time_res_spectrum_data.py
@@ -6,43 +6,33 @@ from datetime import datetime
 import xarray as xr
 from preprocess_mgf_epoch import interpolate_mgf_epoch, convert_epoch
 
-def calc_angle_b0_antenna(b0_dataarray):
+
+def calc_angle_b0_antenna(b0_dataarray,
+                          component: str,
+                          antenna_vector: np.ndarray):
     # b0xryのB0_spinのデータを配列として取り出す。
     b0_spin = b0_dataarray.values
-    #B0_spinの配列の隣同士の要素の外積を計算し、その結果の配列をつくる。配列の最後は1つ前と同じ値とする。
+    # B0_spinの配列の隣同士の要素の外積を計算し、その結果の配列をつくる。配列の最後は1つ前と同じ値とする。
     b0_cross = np.cross(b0_spin[:-1], b0_spin[1:])
     b0_cross = np.append(b0_cross, b0_cross[-1].reshape(1, 3), axis=0)
 
-    #b0_spinとEy_antenna_vectorの外積を計算する。
-    b0_cross_Ey = np.cross(b0_spin, np.array([-np.sin(np.deg2rad(35)),
-                                                np.cos(np.deg2rad(35)),
-                                                0]))
-    #b0_spinとsBy_antenna_vectorの外積を計算する。
-    b0_cross_sBy = np.cross(b0_spin, np.array([0.0, -1.0, 0.0]))
-    
-    # b0_spinとEy_antenna_vectorの内積を計算する。
-    dot_b0_Ey = np.einsum('ij,j->i', b0_spin, np.array([-np.sin(np.deg2rad(35)),
-                                                np.cos(np.deg2rad(35)),
-                                                0]))
-    # b0_spinとsBy_antenna_vectorの内積を計算する。
-    dot_b0_sBy = np.einsum('ij,j->i', b0_spin, np.array([0.0, -1.0, 0.0]))
-    
+    # b0_spinとantenna_vectorの外積を計算する。
+    b0_cross_antenna = np.cross(b0_spin, antenna_vector)
+
+    # b0_spinとantenna_vectorの内積を計算する。
+    dot_b0_antenna = np.einsum('ij,j->i', b0_spin, antenna_vector)
+
     # dot_b0_Eyのarccosを計算する。
-    angle_b0_Ey = np.arccos(dot_b0_Ey/np.linalg.norm(b0_spin, axis=1))
-    # dot_b0_sByのarccosを計算する。
-    angle_b0_sBy = np.arccos(dot_b0_sBy/np.linalg.norm(b0_spin, axis=1))
-    
-    #b0_crossとb0_cross_Eyの内積を計算する。np.einsumは、アインシュタインの縮約記法を用いて、複雑な線形代数演算を表現するための強力な関数
-    dot_b0_cross_b0_cross_Ey = np.einsum('ij,ij->i', b0_cross, b0_cross_Ey)
-    #b0_crossとb0_cross_sByの内積を計算する。
-    dot_b0_cross_b0_cross_sBy = np.einsum('ij,ij->i', b0_cross, b0_cross_sBy)
-    
-    # dot_b0_cross_b0_cross_Eyがマイナスの時は、angle_b0_Eyをマイナスにする。
-    angle_b0_Ey[dot_b0_cross_b0_cross_Ey < 0] = -angle_b0_Ey[dot_b0_cross_b0_cross_Ey < 0]
-    # dot_b0_cross_b0_cross_sByがマイナスの時は、angle_b0_sByをマイナスにする。
-    angle_b0_sBy[dot_b0_cross_b0_cross_sBy < 0] = -angle_b0_sBy[dot_b0_cross_b0_cross_sBy < 0]
-    
-    return xr.DataArray(np.rad2deg(angle_b0_Ey), dims='Epoch', name='angle_b0_Ey'), xr.DataArray(np.rad2deg(angle_b0_sBy), dims='Epoch', name='angle_b0_sBy')
+    angle_b0_antenna = np.arccos(dot_b0_antenna/np.linalg.norm(b0_spin, axis=1))
+
+    # b0_crossとb0_cross_antennaの内積を計算する。np.einsumは、アインシュタインの縮約記法を用いて
+    # 複雑な線形代数演算を表現するための強力な関数
+    dot_b0_cross_b0_cross_antenna = np.einsum('ij,ij->i', b0_cross, b0_cross_antenna)
+
+    # dot_b0_cross_b0_cross_antennaがマイナスの時は、angle_b0_Eyをマイナスにする。
+    angle_b0_antenna[dot_b0_cross_b0_cross_antenna < 0] = -angle_b0_antenna[dot_b0_cross_b0_cross_antenna < 0]
+
+    return xr.DataArray(np.rad2deg(angle_b0_antenna), dims='Epoch', name='angle_b0_'+component)
 
 
 def get_mgf_with_angle_xry(date: str = '1990-2-25'):
@@ -58,7 +48,7 @@ def get_mgf_with_angle_xry(date: str = '1990-2-25'):
     # cdfファイルを読み込む
     cdf_name = '../akebono_data/mgf/ak_h0_mgf_'+date+'_v01.cdf'
     mgf_xary = cdflib.cdf_to_xarray(cdf_name)
-    
+
     b0_spin_da = mgf_xary['B0_spin']
     # mean over dim1
     mean_b0_spin_da = b0_spin_da.mean(dim='dim1')
@@ -72,10 +62,17 @@ def get_mgf_with_angle_xry(date: str = '1990-2-25'):
     mean_b0_spin_da_replaced['Epoch'] = convert_epoch(epoch)
 
     # calculate angle between B0 and antennas
-    angle_b0_Ey, angle_b0_sBy = calc_angle_b0_antenna(mean_b0_spin_da_replaced)
+    angle_b0_Ey = calc_angle_b0_antenna(mean_b0_spin_da_replaced, 'Ey',
+                                        np.array([-np.sin(np.deg2rad(35)),
+                                                  np.cos(np.deg2rad(35)),
+                                                  0]))
+    angle_b0_sBy = calc_angle_b0_antenna(mean_b0_spin_da_replaced, 'sBy',
+                                         np.array([0, -1.0, 0]))
+    angle_b0_Bloop = calc_angle_b0_antenna(mean_b0_spin_da_replaced, 'Bloop',
+                                           np.array([2**-0.5, -3**-0.5, 6**-0.5]))
     # make new dataset from angle_b0_Ey and angle_b0_sBy
-    new_mgf_xry = xr.merge([mean_b0_spin_da_replaced, angle_b0_Ey, angle_b0_sBy])
-    
+    new_mgf_xry = xr.merge([mean_b0_spin_da_replaced, angle_b0_Ey, angle_b0_sBy, angle_b0_Bloop])
+
     return new_mgf_xry
 
 
@@ -103,5 +100,5 @@ def store_mca_high_time_res_data(date: str = '1990-02-25',
     date = datetime.strptime(date, '%Y-%m-%d').strftime('%Y%m%d')
 
     mca_cdf_name = '../akebono_data/vlf/mca/h1/ave0.5s/1990/ak_h1_mca_'+date+'_v02.cdf'
-    pytplot.cdf_to_tplot(mca_cdf_name, prefix='akb_mca_',get_metadata=True)
+    pytplot.cdf_to_tplot(mca_cdf_name, prefix='akb_mca_', get_metadata=True)
     akebono.mca_postprocessing(datatype=datatype, del_invalid_data=del_invalid_data)
