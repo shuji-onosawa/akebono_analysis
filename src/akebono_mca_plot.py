7da63287407224f473aa7b13b69050095197987f
onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2023-06-26 19:38:21 +0900

    add program for plotting high-res mca in limited region

--- src/akebono_mca_plot.py
+++ src/akebono_mca_plot.py
@@ -22,7 +22,7 @@ for i in range(0, 5):
     day_list.append(pyspedas.time_string(time_double, fmt='%Y-%m-%d %H:%M:%S'))
 
 for k in range(len(day_list)-1):
-    
+
     trange = [day_list[k], day_list[k+1]]
     print(trange)
     try:
@@ -36,7 +36,7 @@ for k in range(len(day_list)-1):
         print(e)
         #with open('./akebono_orbit_error_day_list.txt', mode="a") as f:
         #       f.write(trange[0] + '\n')
-        
+
         #print('orbit file does not exists')
         continue
     try:
@@ -58,7 +58,7 @@ for k in range(len(day_list)-1):
                   IMFz]
     IMF_matrix = np.array(IMF_matrix).T
     pytplot.store_data('IMF', data = {'x':time, 'y':IMF_matrix})
-    
+
     tplot_names = ['Emax', 'Eave', 'Bmax', 'Bave']
 
     #dB to amplitude
@@ -72,8 +72,8 @@ for k in range(len(day_list)-1):
         tplot_variable_power = (10**(tplot_variable_float/10)) * ((tplot_variable_0dB)**2)  / bandwidth
         pytplot.store_data(tplot_names[i] +'_Amp', data={'x': tplot_variable.times, 'y': tplot_variable_amplitude, 'v': tplot_variable.v})
         pytplot.store_data(tplot_names[i] +'_Pwr', data={'x': tplot_variable.times, 'y': tplot_variable_power, 'v': tplot_variable.v})
-    
-    
+
+
     #Time interpolate
     try:
         pyspedas.tinterpol('akb_ILAT', interp_to='Emax_Pwr', newname = 'ILAT')
@@ -89,7 +89,7 @@ for k in range(len(day_list)-1):
     pyspedas.tinterpol('akb_Bmdl_X', interp_to='Emax_Pwr', newname = 'Bmdl_X')
     pyspedas.tinterpol('akb_Bmdl_Y', interp_to='Emax_Pwr', newname = 'Bmdl_Y')
     pyspedas.tinterpol('akb_Bmdl_Z', interp_to='Emax_Pwr', newname = 'Bmdl_Z')
-    
+
     #Limit ILAT range
     Emax = get_data('Emax_Pwr')
     time = Emax.times
@@ -100,7 +100,7 @@ for k in range(len(day_list)-1):
     MLT = get_data('MLT')
     MLT = MLT.y
 
-    north_index_tuple = np.where((MLAT>0) & (ILAT>ILAT_min) ) 
+    north_index_tuple = np.where((MLAT>0) & (ILAT>ILAT_min) )
     south_index_tuple = np.where((MLAT<0) & (ILAT>ILAT_min) )
 
     north_index = north_index_tuple[0]
@@ -111,14 +111,14 @@ for k in range(len(day_list)-1):
     if len(north_index) == 0:
         north_start_time_list = None
         north_end_time_list = None
-    else:    
+    else:
         north_start_time_index = [north_index[0]]
         north_end_time_index = []
         for i in range(north_index.size-1):
             if north_index[i+1] - north_index[i] > 1:
                 north_end_time_index.append(north_index[i])
                 north_start_time_index.append(north_index[i+1])
-                
+
         north_end_time_index.append(north_index[-1])
 
         north_start_time_index = np.array(north_start_time_index)
@@ -137,7 +137,7 @@ for k in range(len(day_list)-1):
             if south_index[i+1] - south_index[i] > 1:
                 south_end_time_index.append(south_index[i])
                 south_start_time_index.append(south_index[i+1])
-                
+
         south_end_time_index.append(south_index[-1])
 
         south_start_time_index = np.array(south_start_time_index)
@@ -151,7 +151,7 @@ for k in range(len(day_list)-1):
     end_time_list_list = [north_end_time_list, south_end_time_list]
 
 
-    
+
 
     #make Passname list corresponding with start(end) time list
     Passname = get_data('Pass')
@@ -162,13 +162,13 @@ for k in range(len(day_list)-1):
         north_Passname_list = Passname[north_start_time_index]
     if len(south_index) == 0:
         south_Passname_list = None
-    else:       
+    else:
         south_Passname_list = Passname[south_start_time_index]
 
     Passname_list_list = [north_Passname_list, south_Passname_list]
-    
 
-    #ion gyro freq 
+
+    #ion gyro freq
     Bx = pytplot.get_data('Bmdl_X')
     By = pytplot.get_data('Bmdl_Y')
     Bz = pytplot.get_data('Bmdl_Z')
@@ -189,7 +189,7 @@ for k in range(len(day_list)-1):
     options('gyro_freq', 'ytitle', 'Frequency [Hz]')
 
 
-    #calc power law alpha 
+    #calc power law alpha
     def reg1dim(x, y):
         n = x.size
         a = ((np.dot(x, y)- np.sum(y) * np.sum(x)/n)/
@@ -212,7 +212,7 @@ for k in range(len(day_list)-1):
     for j in range(times.size):
         fit_Emax_low = np.log10(Emax[j][2:7])
         a, b = reg1dim(fit_frange_low, fit_Emax_low)
-        alpha_list_low_freq.append(a)       
+        alpha_list_low_freq.append(a)
         p0 = 10**b
         Emax_lsm = p0*freq**a
         Emax_res = (np.log10(Emax[j]) - np.log10(Emax_lsm)).tolist()
@@ -225,7 +225,7 @@ for k in range(len(day_list)-1):
     store_data('Emax_alpha', data={'x':times, 'y':np.array([alpha_list_low_freq, alpha_list_high_freq]).T})
     options('Emax_alpha', 'legend_names', [r'$\alpha -low$',r'$\alpha -high$'])
     options('Emax_alpha', 'yrange', [-5, 0])
-    
+
     store_data('Emax_pwr_res', data={'x':times, 'y':Emax_res_list, 'v':freq})
     options('Emax_pwr_res', 'spec', 1)
     options('Emax_pwr_res', 'ylog', 1)
@@ -237,17 +237,17 @@ for k in range(len(day_list)-1):
     #make tplot vars of Electric field Amplitude at 3.16 - 1000 Hz
     Eamp = get_data('Emax_Amp')
     Eamp_0_1000Hz = Eamp.y.T[0:11]
-    
-    store_data(name = 'Emax_lines_Amp', 
+
+    store_data(name = 'Emax_lines_Amp',
                data={'x': time,'y': Eamp_0_1000Hz.T})
 
     #make tplot vars of Electric field Amplitude at 3.16 - 1000 Hz
     Epwr = get_data('Emax_Pwr')
     Epwr_0_1000Hz = Epwr.y.T[0:11]
-    
-    store_data(name = 'Emax_lines_Pwr', 
+
+    store_data(name = 'Emax_lines_Pwr',
                data={'x': time,'y': Epwr_0_1000Hz.T})
- 
+
 
     #dir_list = ['./akb_North_mca_plot/', './akb_South_mca_plot/']
     dir_list = ['./plots/akb_North_mca_w_gyro_plot/', './plots/akb_South_mca_w_gyro_plot/']
@@ -282,16 +282,16 @@ for k in range(len(day_list)-1):
             Passname = Passname_list[j]
             Passname = str(int(Passname))
             Passname = Passname[-4:]
-            
+
             #dict event case
             Emax_pwr = get_data('Emax_Pwr')
-            index_tuple = np.where((pyspedas.time_double(start_time_list[j]) < Emax_pwr.times) 
+            index_tuple = np.where((pyspedas.time_double(start_time_list[j]) < Emax_pwr.times)
                                  & (Emax_pwr.times < pyspedas.time_double(end_time_list[j]))
                                  & (MLT>=10) & (MLT<=14))
             Emax_10Hz = Emax_pwr.y.T[2][index_tuple[0]]
-            
+
             save_name = ''
-            
+
             if Emax_10Hz.size == 0:
                 continue
             if np.isnan(np.nanmax(Emax_10Hz)):
@@ -301,7 +301,7 @@ for k in range(len(day_list)-1):
             elif np.nanmax(Emax_10Hz) >=0.3:
                 save_name = dir + 'strong_event/' + 'akb-mca-'+ hemisphere +'_'+ year + Month + day + '_' + hour + minute + second
             elif np.nanmax(Emax_10Hz) >=0.01:
-                save_name = dir + 'normal_event/' + 'akb-mca-'+ hemisphere +'_'+ year + Month + day + '_' + hour + minute + second 
+                save_name = dir + 'normal_event/' + 'akb-mca-'+ hemisphere +'_'+ year + Month + day + '_' + hour + minute + second
 
             tlimit([start_time, end_time])
             options(['Emax_' + surfix, 'Bmax_' + surfix], 'spec', 1)
@@ -330,13 +330,13 @@ for k in range(len(day_list)-1):
                 options('Bmax_' + surfix, 'ztitle', '$[pT^2/Hz]$')
                 options('Emax_lines_' + surfix, 'ysubtitle', '$[(mV/m)^2/Hz]$')
 
-            
-            
+
+
             options('ALT', 'ytitle', 'ALT [km]')
             options('MLT', 'ytitle', 'MLT [h]')
             options('ILAT', 'ytitle', 'ILAT [deg]')
             options(['ALT', 'MLT', 'ILAT'], 'panel_size', 0.3)
-            
+
             omni_data_names = ['SYM_H', 'IMF', 'flow_speed', 'proton_density', 'Pressure', 'E']
             options(omni_data_names, 'panel_size', 0.5)
             options('IMF', 'legend_names', ['IMF x', "IMF y", "IMF z"])
@@ -349,17 +349,17 @@ for k in range(len(day_list)-1):
             options('Pressure', 'ytitle', 'flow \n pressure')
             options('E', 'ytitle', 'E_sw')
             options('E', 'ysubtitle', 'mV/m')
-            
+
             tplot_options('title', Passname + hemisphere + '_' + year+Month+day+ ' MCA ' + surfix)
 
-        
-            tplot(['IMF', 'flow_speed', 'proton_density', 'Pressure', 'gyro_freq', 'Bmax_' + surfix, 'Emax_' + surfix, 'Emax_pwr_res', 'Emax_alpha', 'Emax_lines_' + surfix, 'SYM_H'], 
-                  var_label = ['ALT', 'MLT', 'ILAT'], 
+
+            tplot(['IMF', 'flow_speed', 'proton_density', 'Pressure', 'gyro_freq', 'Bmax_' + surfix, 'Emax_' + surfix, 'Emax_pwr_res', 'Emax_alpha', 'Emax_lines_' + surfix, 'SYM_H'],
+                  var_label = ['ALT', 'MLT', 'ILAT'],
                   save_png = save_name,
                   xsize=26, ysize=22,
                   display=False)
-            
-                
+
+
     tplot_names = pytplot.tplot_names(True)
     pytplot.store_data(tplot_names, delete=True)
     print(pytplot.tplot_names())
---
+++ src/plot_high_res_mca.py
@@ -0,0 +1,69 @@
+import numpy as np
+import xarray as xr
+import os
+import pytplot
+from calc_pwr_matrix_angle_vs_freq import make_wave_mgf_dataset
+from utilities import get_next_date
+import akebono
+
+date = '1990-2-1'
+next_date = get_next_date(date)
+
+make_wave_mgf_dataset(date=date,
+                      mca_datatype='pwr')
+akebono.orb(trange=[date, next_date])
+
+ilat_ds = pytplot.get_data('akb_orb_inv', xarray=True)
+mlt_ds = pytplot.get_data('akb_orb_MLT', xarray=True)
+ds = xr.merge([ilat_ds, mlt_ds])
+
+# MLTとILATの範囲を指定
+target_mlt_range = [10, 14]  # MLTの開始値と終了値を指定
+target_ilat_range = [70, 80]  # ILATの開始値と終了値を指定
+
+# 条件を指定してデータをフィルタリング
+filtered_data = ds.where((ds['akb_orb_inv'] >= target_ilat_range[0]) &
+                         (ds['akb_orb_inv'] <= target_ilat_range[1]) &
+                         (ds['akb_orb_MLT'] >= target_mlt_range[0]) &
+                         (ds['akb_orb_MLT'] <= target_mlt_range[1]),
+                         drop=True)
+
+
+# 時刻データをnumpy配列として取得
+time_array = filtered_data['time'].values
+
+# 時刻の差分を計算
+time_diff = np.diff(time_array)
+
+# 差分が30秒以上のインデックスを取得
+break_indices = np.where(time_diff > np.timedelta64(30, 's'))[0] + 1
+
+# 最初と最後の時刻のインデックスを取得
+start_indices = np.insert(break_indices, 0, 0)
+end_indices = break_indices - 1
+
+# 最初と最後の時刻を取得
+start_times = time_array[start_indices]
+end_times = time_array[end_indices]
+end_times = np.append(end_times, time_array[-1])
+
+# 結果の表示
+print("最初の時刻:", start_times)
+print("最後の時刻:", end_times)
+
+save_dir = '../plots/high_res_mca/'
+os.makedirs(save_dir, exist_ok=True)
+
+for start_time, end_time in zip(start_times, end_times):
+    # start_time, end_timeを'YYYY-MM-DD HH:MM:SS'の形式に変換
+    start_time = start_time.astype('M8[s]').astype(str)
+    end_time = end_time.astype('M8[s]').astype(str)
+
+    # fig_name
+    fig_name = start_time.replace(':', '').replace('-', '').replace(' ', '')
+    print(start_time, end_time)
+    pytplot.tlimit([start_time, end_time])
+    pytplot.tplot(['akb_mca_Emax_pwr', 'akb_mca_Bmax_pwr'],
+                  var_label=['akb_orb_inv', 'akb_orb_MLT', 'akb_orb_ALT'],
+                  xsize=14, ysize=8,
+                  save_jpeg=save_dir + fig_name + '.jpg')
