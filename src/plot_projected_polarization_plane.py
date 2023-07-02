f016795505ae3f6f682d641c5133d04584528106
shuji-onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2023-07-03 06:27:50 +0900

    feat: Add support for plotting projected polarization plane fix: Correct spin plane normal vector style: Format code to improve readability refactor: Rename variables for clarity test: Add tests to ensure correct plotting of projected polarization plane chore: Update plot configuration for projected polarization plane

--- src/plot_projected_polarization_plane.py
+++ src/plot_projected_polarization_plane.py
@@ -16,8 +16,8 @@ def plot_projected_polarization_plane(theta, phi, wna, freq, mode='l'):
     phi_rad = np.deg2rad(phi)
 
     spin_plane_normal_vec = np.array([np.sin(theta_rad)*np.cos(phi_rad),
-                                    np.sin(theta_rad)*np.sin(phi_rad),
-                                    np.cos(theta_rad)])
+                                      np.sin(theta_rad)*np.sin(phi_rad),
+                                      np.cos(theta_rad)])
     spin_plane_unit_vec1 = np.array([np.cos(theta_rad)*np.cos(phi_rad),
                                     np.cos(theta_rad)*np.sin(phi_rad),
                                     -np.sin(theta_rad)])
@@ -28,11 +28,6 @@ def plot_projected_polarization_plane(theta, phi, wna, freq, mode='l'):
     antennna_vec = np.array([np.cos(phase)*spin_plane_unit_vec1[0] + np.sin(phase)*spin_plane_unit_vec2[0],
                             np.cos(phase)*spin_plane_unit_vec1[1] + np.sin(phase)*spin_plane_unit_vec2[1],
                             np.cos(phase)*spin_plane_unit_vec1[2] + np.sin(phase)*spin_plane_unit_vec2[2]])
-
-    # calc angle between z axis and antenna vector
-    angle = np.arccos(np.dot(antennna_vec.T, np.array([0, 0, 1]))/np.linalg.norm(antennna_vec, axis=0))
-    # if phase > 180, angle = -angle
-    angle = np.where(phase > np.pi, -angle, angle)
 
     # wave polarization plane
     wna = wna
@@ -52,16 +47,20 @@ def plot_projected_polarization_plane(theta, phi, wna, freq, mode='l'):
     fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(221, projection='3d')
     ax.quiver(0, 0, 0,
-                spin_plane_normal_vec[0], spin_plane_normal_vec[1], spin_plane_normal_vec[2],
-                color='k', arrow_length_ratio=0.1)
+              spin_plane_normal_vec[0], spin_plane_normal_vec[1], spin_plane_normal_vec[2],
+              color='k', arrow_length_ratio=0.1)
+    ax.quiver(0, 0, 0,
+              spin_plane_unit_vec1[0], spin_plane_unit_vec1[1], spin_plane_unit_vec1[2],
+              color='g', arrow_length_ratio=0.1)
+    ax.quiver(0, 0, 0,
+              spin_plane_unit_vec2[0], spin_plane_unit_vec2[1], spin_plane_unit_vec2[2],
+              color='y', arrow_length_ratio=0.1)
     ax.scatter(np.cos(phase), -Ey_Ex*np.sin(phase), Ez_Ex*np.cos(phase),
-                color='r', s=1.0, marker='o', label='E field, WNA = '+str(wna)+' [deg]')
+               color='r', s=1.0, marker='o', label='E field, WNA = '+str(wna)+' [deg]')
     ax.quiver(0, 0, 0, k_vec[0], k_vec[1], k_vec[2])
     ax.scatter(np.cos(phase), -By_Bx*np.sin(phase), Bz_Bx*np.cos(phase),
-                color='b', s=1.0, marker='o', label='M field, WNA = '+str(wna)+' [deg]')
+               color='b', s=1.0, marker='o', label='M field, WNA = '+str(wna)+' [deg]')
     ax.scatter(2*antennna_vec[0], 2*antennna_vec[1], 2*antennna_vec[2], color='k', s=1, label='antenna vector')
-    Evec = np.array([np.cos(phase), -Ey_Ex*np.sin(phase), Ez_Ex*np.cos(phase)]).T
-    Bvec = np.array([np.cos(phase), -By_Bx*np.sin(phase), Bz_Bx*np.cos(phase)]).T
 
     ax.set_xlim(-2.5, 2.5)
     ax.set_ylim(-2.5, 2.5)
@@ -74,40 +73,60 @@ def plot_projected_polarization_plane(theta, phi, wna, freq, mode='l'):
     ax.legend()
 
     # projection of wave polarization plane on spin plane
+    Evec = np.array([np.cos(phase), -Ey_Ex*np.sin(phase), Ez_Ex*np.cos(phase)]).T
+    Bvec = np.array([np.cos(phase), -By_Bx*np.sin(phase), Bz_Bx*np.cos(phase)]).T
+    # calc projection of E field on spin plane
+    Evec_proj_vec = Evec - np.dot(Evec, spin_plane_normal_vec.reshape(3, 1))*spin_plane_normal_vec.reshape(1, 3)
+    # calc projection of B field on spin plane
+    Bvec_proj_vec = Bvec - np.dot(Bvec, spin_plane_normal_vec.reshape(3, 1))*spin_plane_normal_vec.reshape(1, 3)
+    
     ax = fig.add_subplot(222)
-    Ecomp1 = np.dot(Evec, spin_plane_unit_vec1)
-    Ecomp2 = np.dot(Evec, spin_plane_unit_vec2)
-    ax.scatter(Ecomp1, Ecomp2, color='r', s=1)
-    Bcomp1 = np.dot(Bvec, spin_plane_unit_vec1)
-    Bcomp2 = np.dot(Bvec, spin_plane_unit_vec2)
-    ax.scatter(Bcomp1, Bcomp2, color='b', s=1)
-
+    ax.scatter(np.dot(Evec_proj_vec, spin_plane_unit_vec1),
+               np.dot(Evec_proj_vec, spin_plane_unit_vec2),
+               color='r', s=1)
+    ax.scatter(np.dot(Bvec_proj_vec, spin_plane_unit_vec1),
+               np.dot(Bvec_proj_vec, spin_plane_unit_vec2),
+               color='b', s=1)
     ax.set_xlim(-2.5, 2.5)
     ax.set_ylim(-2.5, 2.5)
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
+    ax.set_xlabel('green')
+    ax.set_ylabel('yellow')
     ax.set_aspect('equal')
 
-    # angle between z axis and antenna vector
-    ax = fig.add_subplot(223)
-    ax.scatter(angle/np.pi*180, phase/np.pi*180, color='black', linewidth=0.5)
-    ax.set_ylabel('phase')
-    ax.set_xlabel('angle')
+    # angle between z axis and field vectors
+    # calc angle between z axis and E field vectors
+    normarized_Evec_proj_vec = Evec_proj_vec/np.linalg.norm(Evec_proj_vec, axis=1).reshape(-1, 1)
+    angle_E_b0 = np.arccos(np.dot(normarized_Evec_proj_vec, np.array([0, 0, 1]).reshape(3, 1)))
+    for i in range(len(angle_E_b0)):
+        if np.dot(normarized_Evec_proj_vec[i], spin_plane_unit_vec2) < 0:
+            angle_E_b0[i] = -angle_E_b0[i]
+    # calc angle between z axis and B field vectors
+    normarized_Bvec_proj_vec = Bvec_proj_vec/np.linalg.norm(Bvec_proj_vec, axis=1).reshape(-1, 1)
+    angle_B_b0 = np.arccos(np.dot(normarized_Bvec_proj_vec, np.array([0, 0, 1]).reshape(3, 1)))
+    for i in range(len(angle_B_b0)):
+        if np.dot(normarized_Bvec_proj_vec[i], spin_plane_unit_vec2) < 0:
+            angle_B_b0[i] = -angle_B_b0[i]
 
-    # angle dependence of power
-    ax = fig.add_subplot(224)
-    Epwr = Ecomp1**2+Ecomp2**2
-    normarized_Epwr = Epwr/np.max(Epwr)
-    Bpwr = Bcomp1**2+Bcomp2**2
-    normarized_Bpwr = Bpwr/np.max(Bpwr)
-    ax.scatter(angle/np.pi*180, normarized_Epwr, color='r', linewidth=0.5, label='E field')
-    ax.scatter(angle/np.pi*180, normarized_Bpwr, color='b', linewidth=0.5, label='B field')
+    # plot angle dependence of power
+    # calc field vector norm
+    Evec_proj_vec_norm = np.linalg.norm(Evec_proj_vec, axis=1)
+    Bvec_proj_vec_norm = np.linalg.norm(Bvec_proj_vec, axis=1)
+
+    ax = fig.add_subplot(223)
+    ax.scatter(np.rad2deg(angle_E_b0), Evec_proj_vec_norm/np.nanmax(Evec_proj_vec_norm),
+               color='r', label='E field', s=2.0)
+    ax.scatter(np.rad2deg(angle_B_b0), Bvec_proj_vec_norm/np.nanmax(Bvec_proj_vec_norm),
+               color='b', label='B field', s=2.0)
+    ax.set_xlim(-180, 180)
+    ax.set_ylim(0, 1.1)
+    ax.vlines([-90, 90], 0, 1.1, color='k', linestyle='dashed')
+
     ax.set_xlabel('angle')
     ax.set_ylabel('power')
     ax.set_xticks([-180, -90, 0, 90, 180])
-    ax.set_yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0])
-    ax.legend()
-
+    # legend position
+    ax.legend(loc='lower right')
+    plt.show
     if mode == 'l':
         savefig_dir = '../plots/projected_polarization_plane/left_hand_circular/freq'+str(freq)+'wna'+str(wna)+'theta'+str(theta)+'/'
     elif mode == 'r':
