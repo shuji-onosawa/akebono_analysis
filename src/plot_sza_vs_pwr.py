8f7ec6f26c6507e41f80643e0b4c9a7f90727a0e
onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2023-08-25 18:16:22 +0900

    feat: Rename 'v_dim' to 'v_dim_geo' in geo_xry feat: Select data within specified ranges in dataset feat: Calculate solar zenith angle (SZA) from geographic position

--- src/plot_sza_vs_pwr.py
+++ src/plot_sza_vs_pwr.py
@@ -47,6 +47,18 @@ for date1, date2 in zip(date_list[:-1], date_list[1:]):
     alt_xry = pytplot.get_data('akb_orb_alt_intrp', xarray=True)
 
     # make dataset
-    geo_xry = geo_xry.rename_dims({'v_dim': 'v_dim1'})
+    geo_xry = geo_xry.rename({'v_dim': 'v_dim_geo'})
     dataset = xr.merge([Epwr_xry, Bpwr_xry, geo_xry, inv_xry, mlt_xry, alt_xry])
-    print(dataset)
+
+    # data selection
+    dataset = dataset.where((dataset['inv'] >= inv_min) & (dataset['inv'] <= inv_max), drop=True)
+    dataset = dataset.where((dataset['mlt'] >= mlt_min) & (dataset['mlt'] <= mlt_max), drop=True)
+    dataset = dataset.where((dataset['alt'] >= alt_min) & (dataset['alt'] <= alt_max), drop=True)
+
+    # calc sza
+    # convert datetime64 to epoch
+    geo_datetime = dataset['akb_orb_geo_intrp'].coords['time'].values
+    geo_epoch = pd.to_datetime(geo_datetime).astype(int) / 10 ** 9
+    geo_pos = dataset['akb_orb_geo_intrp'].values
+    for i in range(len(geo_pos)):
+        sza_list.append(calc_sza_from_geo(geo_pos[i][0], geo_pos[i][1], geo_pos[i][2]))
