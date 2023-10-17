c00648895d6aa4c6d35a6bfe761e3357e5bf1a84
onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2023-10-17 19:52:55 +0900

    feat: Add function to calculate moving average and moving standard deviation
    feat: Add function to store moving average in pytplot
    
    feat: Add function to store moving standard deviation in pytplot
    
    feat: Add function to store MCA power lines in pytplot

--- src/plot_mca_line.py
+++ src/plot_mca_line.py
@@ -1,57 +1,100 @@
 import pytplot
-from store_high_time_res_spectrum_data import store_mca_high_time_res_data
-
+import numpy as np
 '''
 MCAスペクトルデータを読み込み、それぞれの周波数ごとに強度vs.時間のグラフを作成する
-各プロットには、周波数ごとの強度とその1時間平均値、1 sigmaの値を表示する
+各プロットには、周波数ごとの強度(Todo:とその1時間平均値、1 sigmaの値を表示する)
 プロットにはpytplotモジュールを使用する
 '''
 
-# Load data
-date = '1990-2-11'
-store_mca_high_time_res_data(date=date)
 
-epwrXry = pytplot.get_data('akb_mca_Emax_pwr', xarray=True)
-bpwrXry = pytplot.get_data('akb_mca_Bmax_pwr', xarray=True)
-# 時刻を制限する
-startTime = '1990-02-11T16:00:00.000'
-endTime = '1990-02-11T20:00:00.000'
-print(epwrXry)
-epwrXry = epwrXry.sel(time=slice(startTime, endTime))
-bpwrXry = bpwrXry.sel(time=slice(startTime, endTime))
+def calcMovingAverage(xarray, timeWindow):
+    '''
+    時間移動平均を計算する
+    :param xarray: xarray.DataArray
+    :param timeWindow: int
+    :return: xarray.DataArray
+    '''
+    return xarray.rolling(time=timeWindow, center=True, min_periods=1).mean(skipna=True)
 
-# 1時間移動平均、1 sigmaを計算する
-epwrMovingAveXry = epwrXry.rolling(time=7200, center=True, min_periods=1).mean(skipna=True)
-epwrMovingStdXry = epwrXry.rolling(time=7200, center=True, min_periods=1).std(skipna=True)
-bpwrMovingAveXry = bpwrXry.rolling(time=7200, center=True, min_periods=1).mean(skipna=True)
-bpwrMovingAstXry = bpwrXry.rolling(time=7200, center=True, min_periods=1).std(skipna=True)
 
-# store data
-specBin = epwrXry.coords['spec_bins'].values
+def calcMovingStd(xarray, timeWindow):
+    '''
+    時間移動標準偏差を計算する
+    :param xarray: xarray.DataArray
+    :param timeWindow: int
+    :return: xarray.DataArray
+    '''
+    return xarray.rolling(time=timeWindow, center=True, min_periods=1).std(skipna=True)
 
-timeMovingAve = epwrMovingAveXry.coords['time'].values
 
-epwrMovingAveMatrix = epwrMovingAveXry.values
-epwrMovingStdMatrix = epwrMovingStdXry.values
-bpwrMovingAveMatrix = bpwrMovingAveXry.values
-bpwrMovingStdMatrix = bpwrMovingAstXry.values
+def storeMovAverage(xarray, timeWindow, storeName):
+    '''
+    時間移動平均を計算し、pytplotに格納する
+    :param xarray: xarray.DataArray
+    :param timeWindow: int
+    :param storeName: str
+    :return: None
+    '''
+    movingAveXry = calcMovingAverage(xarray, timeWindow)
+    movingAveMatrix = movingAveXry.values
+    timeMovingAve = movingAveXry.coords['time'].values
+    for i in range(movingAveMatrix.shape[1]):
+        pytplot.store_data(storeName, data={'x': timeMovingAve, 'y': movingAveMatrix[:, i].T})
+        pytplot.options(storeName, 'ytitle', 'Moving Average ch{} \n[(mV/m)^2/Hz]'.format(i+1))
 
-for i in range(specBin.size):
-    storeNameEpwrMovAve = 'akb_mca_Epwr_1h_mov_ave_ch{}'.format(i+1)
-    storeNameEpwrMovStd = 'akb_mca_Epwr_1h_mov_std_ch{}'.format(i+1)
-    storeNameBpwrMovAve = 'akb_mca_Bpwr_1h_mov_ave_ch{}'.format(i+1)
-    storeNameBpwrMovStd = 'akb_mca_Bpwr_1h_mov_std_ch{}'.format(i+1)
 
-    pytplot.store_data(storeNameEpwrMovAve, data={'x': timeMovingAve, 'y': epwrMovingAveMatrix[:, i].T})
-    pytplot.store_data(storeNameEpwrMovStd, data={'x': timeMovingAve, 'y': epwrMovingStdMatrix[:, i].T})
-    pytplot.store_data(storeNameBpwrMovAve, data={'x': timeMovingAve, 'y': bpwrMovingAveMatrix[:, i].T})
-    pytplot.store_data(storeNameBpwrMovStd, data={'x': timeMovingAve, 'y': bpwrMovingStdMatrix[:, i].T})
+def storeMovStd(xarray, timeWindow, storeName):
+    '''
+    時間移動標準偏差を計算し、pytplotに格納する
+    :param xarray: xarray.DataArray
+    :param timeWindow: int
+    :param storeName: str
+    :return: None
+    '''
+    movingStdXry = calcMovingStd(xarray, timeWindow)
+    movingStdMatrix = movingStdXry.values
+    timeMovingStd = movingStdXry.coords['time'].values
+    for i in range(movingStdMatrix.shape[1]):
+        pytplot.store_data(storeName, data={'x': timeMovingStd, 'y': movingStdMatrix[:, i].T})
+        pytplot.options(storeName, 'ytitle', 'Moving Std ch{} \n[(mV/m)^2/Hz]'.format(i+1))
 
-    pytplot.options(storeNameEpwrMovAve, 'ytitle', 'Epwr ch{} \n[(mV/m)^2/Hz]'.format(i+1))
-    pytplot.options(storeNameEpwrMovStd, 'ytitle', 'Epwr ch{} \n[(mV/m)^2/Hz]'.format(i+1))
-    pytplot.options(storeNameBpwrMovAve, 'ytitle', 'Bpwr ch{} \n[(pT)^2/Hz]'.format(i+1))
-    pytplot.options(storeNameBpwrMovStd, 'ytitle', 'Bpwr ch{} \n[(pT)^2/Hz]'.format(i+1))
 
-# plot
-pytplot.tlimit(['1990-2-11 16:00:00', '1990-2-11 20:00:00'])
-pytplot.tplot(['akb_mca_Emax_pwr', 'akb_mca_Epwr_1h_mov_ave_ch1'])
+def storeEpwrLines(xarray):
+    '''
+    xarray.DataArrayをpytplotに格納する
+    :param xarray: xarray.DataArray
+    :param storeName: str
+    :return: None
+    '''
+    pwrMatrix = xarray.values
+    timePwr = xarray.coords['time'].values
+    specBin = xarray.coords['spec_bins'].values
+    # 各周波数の最大値、最小値を取得
+    pwrMax = np.nanmax(pwrMatrix, axis=0)
+    pwrMin = np.nanmin(pwrMatrix, axis=0)
+    print(pwrMax.shape, pwrMin.shape)
+    for i in range(pwrMatrix.shape[1]):
+        storeName = 'akb_mca_Epwr_ch{}'.format(i+1)
+        pytplot.store_data(storeName, data={'x': timePwr, 'y': pwrMatrix[:, i].T})
+        pytplot.options(storeName, 'ytitle', '{} \n[(mV/m)^2/Hz]'.format(str(specBin[i])))
+        pytplot.options(storeName, 'yrange', [0.9*pwrMin[i], 1.1*pwrMax[i]])
+
+
+def storeBpwrLines(xarrary):
+    '''
+    xarray.DataArrayをpytplotに格納する
+    :param xarrary: xarray.DataArray
+    :param storeName: str
+    :return: None
+    '''
+    pwrMatrix = xarrary.values
+    timePwr = xarrary.coords['time'].values
+    specBin = xarrary.coords['spec_bins'].values
+    # 各周波数の最大値、最小値を取得
+    pwrMax = np.nanmax(pwrMatrix, axis=0)
+    pwrMin = np.nanmin(pwrMatrix, axis=0)
+    for i in range(pwrMatrix.shape[1]):
+        storeName = 'akb_mca_Bpwr_ch{}'.format(i+1)
+        pytplot.store_data(storeName, data={'x': timePwr, 'y': pwrMatrix[:, i].T})
+        pytplot.options(storeName, 'ytitle', '{} \n[(pT)^2/Hz]'.format(str(specBin[i])))
+        pytplot.options(storeName, 'yrange', [0.9*pwrMin[i], 1.1*pwrMax[i]])
