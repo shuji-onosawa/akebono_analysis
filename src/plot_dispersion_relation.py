4fef9433a9e0b01ad994ba1fd4939904caa195c6
shuji-onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2023-12-01 16:35:13 +0900

    feat: Added function to plot refraction index vs frequency in plot_dispersion_relation.py

--- src/plot_dispersion_relation.py
+++ src/plot_dispersion_relation.py
@@ -48,23 +48,33 @@ def plotFreqVsWavelength(waveNormalAngle, save_dir):
         f.write('Va = '+str(pp.Va)+' [m s^-1]\n')
 
 
+def plotRefractionIndexVsFrequency(waveNormalAngle, save_dir):
+    # before running this script, check plasma parameters in plasma_params.py
+
+    # set parameters
+    omega_s = pp.wlh
+    freq = np.abs(omega_s['value'])*np.logspace(-3, 1, 10000)
+
+    # calc dispersion relation
+    n_L, n_R, S, D, P = calc_dr.calc_dispersion_relation(freq, waveNormalAngle)
+
+    # plot refraction index vs frequency
+    fig, axs = plt.subplots(nrows=1, ncols=1, figsize=[8, 8])
+    axs.scatter(x=freq, y=n_L, label=r'$L mode$', c='r', marker='.')
+    axs.scatter(x=freq, y=n_R, label=r'$R mode$', c='b', marker='.')
+    axs.vlines([pp.omega_he['value'], pp.omega_h['value'], pp.omega_o['value'], -pp.omega_e['value'], pp.wlh['value']], 0, 1e7, linestyles='dashed', colors='k')  # charateristic frequencies
+    axs.set_xscale('log')
+    axs.set_yscale('log')
+    axs.set_xlabel(r'$\omega/$'+omega_s['label'])
+    axs.set_ylabel(r'$Refraction index ^2$')
+
+    plt.legend()
+    # make directory if not exist
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    plt.savefig(save_dir+'/refractionIndexVsfrequency_' + str(waveNormalAngle)+'.png')
+
+
 for wna in [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]:
-    save_dir = '../plots/Ishigaya_events/0217/dispersion_relation/'
-    plotFreqVsWavelength(wna, save_dir)
-
-'''
-# plot refraction index vs frequency
-fig, axs = plt.subplots(nrows=1, ncols=1, figsize=[16, 8])
-axs.scatter(x=freq/omega_s['value'], y=n_L, label=r'$L mode$', c='r', marker='.')
-axs.scatter(x=freq/omega_s['value'], y=n_R, label=r'$R mode$', c='b', marker='.')
-axs.vlines(char_freq, 0, 1e7, linestyles='dashed', colors='k')
-# axs.vlines(crossover_freq, 0, 1e7, linestyles='dashed', colors='r')
-axs.set_xscale('log')
-axs.set_yscale('log')
-axs.set_xlabel(r'$\omega/$'+omega_s['label'])
-axs.set_ylabel(r'$Refraction index ^2$')
-
-plt.legend()
-# plt.savefig('../plots/dispersion_relation/refraction_index_wna_'+str(theta)+'.png')
-plt.show()
-'''
+    save_dir = '../plots/'
+    plotRefractionIndexVsFrequency(wna, save_dir)
