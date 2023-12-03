51e263251d306fe21852a58492eb4d916fd89025
Shuji Onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2023-12-04 04:01:37 +0900

    Fix bug in login functionality

--- src/plot_angleB0_vs_pwr_by_each_halfspin.py
+++ src/plot_angleB0_vs_pwr_by_each_halfspin.py
@@ -39,149 +39,74 @@ else:
         halfSpinIdxRangeList.append([posToNegIdx[i] + 1, negToPosIdx[i] + 1])
         halfSpinIdxRangeList.append([negToPosIdx[i] + 1, posToNegIdx[i + 1] + 1])
 
-# Calculate Epwr max and min for each channel
+# Calculate pwr max and min for each channel
 # Max and min are used for ylim of plot
 EpwrMaxArray = subDataset['akb_mca_Emax_pwr'].max(dim='Epoch').values
 EpwrMinArray = subDataset['akb_mca_Emax_pwr'].min(dim='Epoch').values
-
-# Plot angleB0Ey vs. Epwr
-print("Plotting angleB0Ey vs. Epwr")
-for j in range(len(halfSpinIdxRangeList) - 2):
-    if angleB0EyArray[halfSpinIdxRangeList[j][0]] < 0:
-        angleB0EyArray[halfSpinIdxRangeList[j][0]:halfSpinIdxRangeList[j][1]] += 180
-    if angleB0EyArray[halfSpinIdxRangeList[j + 1][0]] < 0:
-        angleB0EyArray[halfSpinIdxRangeList[j + 1][0]:halfSpinIdxRangeList[j + 1][1]] += 180
-    if angleB0EyArray[halfSpinIdxRangeList[j + 2][0]] < 0:
-        angleB0EyArray[halfSpinIdxRangeList[j + 2][0]:halfSpinIdxRangeList[j + 2][1]] += 180
-
-    # epochs of half spin 1, 2, 3
-    epoch1 = Epoch[halfSpinIdxRangeList[j][0]]
-    epoch2 = Epoch[halfSpinIdxRangeList[j + 1][0]]
-    epoch3 = Epoch[halfSpinIdxRangeList[j + 2][0]]
-    # Plot angleB0Ey vs. pwr for each channel
-    for channelIdx in range(12):
-        fig = plt.figure(figsize=(12, 8))
-        ax = fig.add_subplot(111)
-        ax.plot(angleB0EyArray[halfSpinIdxRangeList[j][0]:halfSpinIdxRangeList[j][1]],
-                EpwrArray[halfSpinIdxRangeList[j][0]:halfSpinIdxRangeList[j][1], channelIdx], label=str(epoch1), marker='.')
-        ax.plot(angleB0EyArray[halfSpinIdxRangeList[j + 1][0]:halfSpinIdxRangeList[j + 1][1]],
-                EpwrArray[halfSpinIdxRangeList[j + 1][0]:halfSpinIdxRangeList[j + 1][1], channelIdx], label=str(epoch2), marker='.')
-        ax.plot(angleB0EyArray[halfSpinIdxRangeList[j + 2][0]:halfSpinIdxRangeList[j + 2][1]],
-                EpwrArray[halfSpinIdxRangeList[j + 2][0]:halfSpinIdxRangeList[j + 2][1], channelIdx], label=str(epoch3), marker='.')
-        ax.set_title('Angle between B0 and Ey vs. Power for ' + freqLabel[channelIdx] + '\n' +
-                     str(Epoch[halfSpinIdxRangeList[j][0]]) + ' to ' + str(Epoch[halfSpinIdxRangeList[j + 2][1]]))
-        ax.set_xlabel('Angle between B0 and Ey (deg)')
-        ax.set_ylabel('Power [(mV/m)^2/Hz]')
-        ax.set_xlim(0, 180)
-        ax.set_ylim(EpwrMinArray[channelIdx] * 0.9, EpwrMaxArray[channelIdx] * 1.1)
-        ax.legend()
-
-        # Save plot
-        os.makedirs(saveDir + '/Efiled/' + freqLabel[channelIdx] + '/', exist_ok=True)
-        plt.savefig(saveDir + '/Efiled/' + freqLabel[channelIdx] + '/spin' + str(j) + str(j + 1) + str(j + 2) + '.jpeg')
-print("Finished plotting angleB0Ey vs. Epwr")
-
-# Find half spin index range for B field (sBy)
-posToNegIdx, negToPosIdx = find_zero_cross_idx(angleB0sByArray)
-halfSpinIdxRangeList = []
-
-if posToNegIdx[0] > negToPosIdx[0]:
-    for i in range(len(posToNegIdx) - 1):
-        halfSpinIdxRangeList.append([negToPosIdx[i] + 1, posToNegIdx[i] + 1])
-        halfSpinIdxRangeList.append([posToNegIdx[i] + 1, negToPosIdx[i + 1] + 1])
-else:
-    for i in range(len(posToNegIdx) - 1):
-        halfSpinIdxRangeList.append([posToNegIdx[i] + 1, negToPosIdx[i] + 1])
-        halfSpinIdxRangeList.append([negToPosIdx[i] + 1, posToNegIdx[i + 1] + 1])
-
-# Calculate Bpwr max and min for each channel
-# Max and min are used for ylim of plot
 BpwrMaxArray = subDataset['akb_mca_Bmax_pwr'].max(dim='Epoch').values
 BpwrMinArray = subDataset['akb_mca_Bmax_pwr'].min(dim='Epoch').values
 
-# Plot angleB0sBy vs. Bpwr
-print("Plotting angleB0sBy vs. Bpwr")
-for j in range(len(halfSpinIdxRangeList) - 2):
-    if angleB0sByArray[halfSpinIdxRangeList[j][0]] < 0:
-        angleB0sByArray[halfSpinIdxRangeList[j][0]:halfSpinIdxRangeList[j][1]] += 180
-    if angleB0sByArray[halfSpinIdxRangeList[j + 1][0]] < 0:
-        angleB0sByArray[halfSpinIdxRangeList[j + 1][0]:halfSpinIdxRangeList[j + 1][1]] += 180
-    if angleB0sByArray[halfSpinIdxRangeList[j + 2][0]] < 0:
-        angleB0sByArray[halfSpinIdxRangeList[j + 2][0]:halfSpinIdxRangeList[j + 2][1]] += 180
+# angle*arrayの値が負の場合は180度加算する
+angleB0EyArray[angleB0EyArray < 0] += 180
+angleB0sByArray[angleB0sByArray < 0] += 180
+angleB0BloopArray[angleB0BloopArray < 0] += 180
 
+# Plot angleB0Ey vs. Epwr, angleB0B vs. Bpwr for each half spin
+print("Plotting...")
+for j in range(len(halfSpinIdxRangeList) - 2):
     # epochs of half spin 1, 2, 3
     epoch1 = Epoch[halfSpinIdxRangeList[j][0]]
     epoch2 = Epoch[halfSpinIdxRangeList[j + 1][0]]
     epoch3 = Epoch[halfSpinIdxRangeList[j + 2][0]]
-    # Plot angleB0sBy vs. pwr for each channel
-    for channelIdx in range(10):
-        fig = plt.figure(figsize=(12, 8))
-        ax = fig.add_subplot(111)
-        ax.plot(angleB0sByArray[halfSpinIdxRangeList[j][0]:halfSpinIdxRangeList[j][1]],
-                BpwrArray[halfSpinIdxRangeList[j][0]:halfSpinIdxRangeList[j][1], channelIdx], label=str(epoch1), marker='.')
-        ax.plot(angleB0sByArray[halfSpinIdxRangeList[j + 1][0]:halfSpinIdxRangeList[j + 1][1]],
-                BpwrArray[halfSpinIdxRangeList[j + 1][0]:halfSpinIdxRangeList[j + 1][1], channelIdx], label=str(epoch2), marker='.')
-        ax.plot(angleB0sByArray[halfSpinIdxRangeList[j + 2][0]:halfSpinIdxRangeList[j + 2][1]],
-                BpwrArray[halfSpinIdxRangeList[j + 2][0]:halfSpinIdxRangeList[j + 2][1], channelIdx], label=str(epoch3), marker='.')
-        ax.set_title('Angle between B0 and sBy vs. Power for ' + freqLabel[channelIdx] + '\n' +
-                     str(Epoch[halfSpinIdxRangeList[j][0]]) + ' to ' + str(Epoch[halfSpinIdxRangeList[j + 2][1]]))
-        ax.set_xlabel('Angle between B0 and sBy (deg)')
-        ax.set_ylabel('Power [pT^2/Hz]')
-        ax.set_xlim(0, 180)
-        ax.set_ylim(BpwrMinArray[channelIdx] * 0.9, BpwrMaxArray[channelIdx] * 1.1)
-        ax.legend()
-
+    # Plot angleB0* vs. *pwr for each channel
+    for channelIdx in range(12):
+        fig, axs = plt.subplots(2, 1, figsize=(12, 8))
+        axs[0].plot(angleB0EyArray[halfSpinIdxRangeList[j][0]:halfSpinIdxRangeList[j][1]],
+                    EpwrArray[halfSpinIdxRangeList[j][0]:halfSpinIdxRangeList[j][1], channelIdx], label=str(epoch1), marker='*', linestyle='None')
+        axs[0].plot(angleB0EyArray[halfSpinIdxRangeList[j + 1][0]:halfSpinIdxRangeList[j + 1][1]],
+                    EpwrArray[halfSpinIdxRangeList[j + 1][0]:halfSpinIdxRangeList[j + 1][1], channelIdx], label=str(epoch2), marker='*', linestyle='None')
+        axs[0].plot(angleB0EyArray[halfSpinIdxRangeList[j + 2][0]:halfSpinIdxRangeList[j + 2][1]],
+                    EpwrArray[halfSpinIdxRangeList[j + 2][0]:halfSpinIdxRangeList[j + 2][1], channelIdx], label=str(epoch3), marker='*', linestyle='None')
+        axs[0].set_title('Angle between B0 and Ey vs. Power for ' + freqLabel[channelIdx] + '\n' +
+                            str(Epoch[halfSpinIdxRangeList[j][0]]) + ' to ' + str(Epoch[halfSpinIdxRangeList[j + 2][1]]))
+        axs[0].set_xlabel('Angle between B0 and Ey (deg)')
+        axs[0].set_ylabel('Power [(mV/m)^2/Hz]')
+        axs[0].set_yscale('log')
+        axs[0].set_xlim(0, 180)
+        axs[0].set_ylim(EpwrMinArray[channelIdx], EpwrMaxArray[channelIdx])
+        axs[0].legend()
+        if channelIdx == 10 or channelIdx == 11:
+            axs[1].plot(angleB0BloopArray[halfSpinIdxRangeList[j][0]:halfSpinIdxRangeList[j][1]],
+                        BpwrArray[halfSpinIdxRangeList[j][0]:halfSpinIdxRangeList[j][1], channelIdx], label=str(epoch1), marker='*', linestyle='None')
+            axs[1].plot(angleB0BloopArray[halfSpinIdxRangeList[j + 1][0]:halfSpinIdxRangeList[j + 1][1]],
+                        BpwrArray[halfSpinIdxRangeList[j + 1][0]:halfSpinIdxRangeList[j + 1][1], channelIdx], label=str(epoch2), marker='*', linestyle='None')
+            axs[1].plot(angleB0BloopArray[halfSpinIdxRangeList[j + 2][0]:halfSpinIdxRangeList[j + 2][1]],
+                        BpwrArray[halfSpinIdxRangeList[j + 2][0]:halfSpinIdxRangeList[j + 2][1], channelIdx], label=str(epoch3), marker='*', linestyle='None')
+            axs[1].set_title('Angle between B0 and Bloop vs. Power for ' + freqLabel[channelIdx] + '\n' +
+                                str(Epoch[halfSpinIdxRangeList[j][0]]) + ' to ' + str(Epoch[halfSpinIdxRangeList[j + 2][1]]))
+            axs[1].set_xlabel('Angle between B0 and Bloop (deg)')
+            axs[1].set_ylabel('Power [pT^2/Hz]')
+            axs[1].set_yscale('log')
+            axs[1].set_xlim(0, 180)
+            axs[1].set_ylim(BpwrMinArray[channelIdx], BpwrMaxArray[channelIdx])
+            axs[1].legend()
+        else:
+            axs[1].plot(angleB0sByArray[halfSpinIdxRangeList[j][0]:halfSpinIdxRangeList[j][1]],
+                        BpwrArray[halfSpinIdxRangeList[j][0]:halfSpinIdxRangeList[j][1], channelIdx], label=str(epoch1), marker='*', linestyle='None')
+            axs[1].plot(angleB0sByArray[halfSpinIdxRangeList[j + 1][0]:halfSpinIdxRangeList[j + 1][1]],
+                        BpwrArray[halfSpinIdxRangeList[j + 1][0]:halfSpinIdxRangeList[j + 1][1], channelIdx], label=str(epoch2), marker='*', linestyle='None')
+            axs[1].plot(angleB0sByArray[halfSpinIdxRangeList[j + 2][0]:halfSpinIdxRangeList[j + 2][1]],
+                        BpwrArray[halfSpinIdxRangeList[j + 2][0]:halfSpinIdxRangeList[j + 2][1], channelIdx], label=str(epoch3), marker='*', linestyle='None')
+            axs[1].set_title('Angle between B0 and sBy vs. Power for ' + freqLabel[channelIdx] + '\n' +
+                                str(Epoch[halfSpinIdxRangeList[j][0]]) + ' to ' + str(Epoch[halfSpinIdxRangeList[j + 2][1]]))
+            axs[1].set_xlabel('Angle between B0 and sBy (deg)')
+            axs[1].set_ylabel('Power [pT^2/Hz]')
+            axs[1].set_yscale('log')
+            axs[1].set_xlim(0, 180)
+            axs[1].set_ylim(BpwrMinArray[channelIdx], BpwrMaxArray[channelIdx])
+            axs[1].legend()
         # Save plot
-        os.makedirs(saveDir + '/Bfiled/' + freqLabel[channelIdx] + '/', exist_ok=True)
-        plt.savefig(saveDir + '/Bfiled/' + freqLabel[channelIdx] + '/spin' + str(j) + str(j + 1) + str(j + 2) + '.jpeg')
-print("Finished plotting angleB0sBy vs. Bpwr")
-
-# Find half spin index range for B field (Bloop)
-posToNegIdx, negToPosIdx = find_zero_cross_idx(angleB0BloopArray)
-halfSpinIdxRangeList = []
-
-if posToNegIdx[0] > negToPosIdx[0]:
-    for i in range(len(posToNegIdx) - 1):
-        halfSpinIdxRangeList.append([negToPosIdx[i] + 1, posToNegIdx[i] + 1])
-        halfSpinIdxRangeList.append([posToNegIdx[i] + 1, negToPosIdx[i + 1] + 1])
-else:
-    for i in range(len(posToNegIdx) - 1):
-        halfSpinIdxRangeList.append([posToNegIdx[i] + 1, negToPosIdx[i] + 1])
-        halfSpinIdxRangeList.append([negToPosIdx[i] + 1, posToNegIdx[i + 1] + 1])
-
-# Plot angleB0Bloop vs. Bpwr
-print("Plotting angleB0Bloop vs. Bpwr")
-for j in range(len(halfSpinIdxRangeList) - 2):
-    if angleB0BloopArray[halfSpinIdxRangeList[j][0]] < 0:
-        angleB0BloopArray[halfSpinIdxRangeList[j][0]:halfSpinIdxRangeList[j][1]] += 180
-    if angleB0BloopArray[halfSpinIdxRangeList[j + 1][0]] < 0:
-        angleB0BloopArray[halfSpinIdxRangeList[j + 1][0]:halfSpinIdxRangeList[j + 1][1]] += 180
-    if angleB0BloopArray[halfSpinIdxRangeList[j + 2][0]] < 0:
-        angleB0BloopArray[halfSpinIdxRangeList[j + 2][0]:halfSpinIdxRangeList[j + 2][1]] += 180
-
-    # epochs of half spin 1, 2, 3
-    epoch1 = Epoch[halfSpinIdxRangeList[j][0]]
-    epoch2 = Epoch[halfSpinIdxRangeList[j + 1][0]]
-    epoch3 = Epoch[halfSpinIdxRangeList[j + 2][0]]
-    # Plot angleB0Bloop vs. pwr for each channel
-    for channelIdx in (10, 11):
-        fig = plt.figure(figsize=(12, 8))
-        ax = fig.add_subplot(111)
-        ax.plot(angleB0BloopArray[halfSpinIdxRangeList[j][0]:halfSpinIdxRangeList[j][1]],
-                BpwrArray[halfSpinIdxRangeList[j][0]:halfSpinIdxRangeList[j][1], channelIdx], label=str(epoch1), marker='.')
-        ax.plot(angleB0BloopArray[halfSpinIdxRangeList[j + 1][0]:halfSpinIdxRangeList[j + 1][1]],
-                BpwrArray[halfSpinIdxRangeList[j + 1][0]:halfSpinIdxRangeList[j + 1][1], channelIdx], label=str(epoch2), marker='.')
-        ax.plot(angleB0BloopArray[halfSpinIdxRangeList[j + 2][0]:halfSpinIdxRangeList[j + 2][1]],
-                BpwrArray[halfSpinIdxRangeList[j + 2][0]:halfSpinIdxRangeList[j + 2][1], channelIdx], label=str(epoch3), marker='.')
-        ax.set_title('Angle between B0 and Bloop vs. Power for ' + freqLabel[channelIdx] + '\n' +
-                     str(Epoch[halfSpinIdxRangeList[j][0]]) + ' to ' + str(Epoch[halfSpinIdxRangeList[j + 2][1]]))
-        ax.set_xlabel('Angle between B0 and Bloop (deg)')
-        ax.set_ylabel('Power [pT^2/Hz]')
-        ax.set_xlim(0, 180)
-        ax.set_ylim(BpwrMinArray[channelIdx] * 0.9, BpwrMaxArray[channelIdx] * 1.1)
-        ax.legend()
-
-        # Save plot
-        os.makedirs(saveDir + '/Bfiled/' + freqLabel[channelIdx] + '/', exist_ok=True)
-        plt.savefig(saveDir + '/Bfiled/' + freqLabel[channelIdx] + '/spin' + str(j) + str(j + 1) + str(j + 2) + '.jpeg')
-print("Finished plotting angleB0Bloop vs. Bpwr")
+        os.makedirs(saveDir + freqLabel[channelIdx] + '/', exist_ok=True)
+        plt.savefig(saveDir + freqLabel[channelIdx] + '/spin' + str(j) +'-'+ str(j + 1) +'-'+ str(j + 2) + '.jpeg')
+        plt.close()
+print("Finished plotting")
