eda647516844408daa23f99337942006a17f81e1
Shuji Onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2023-12-11 13:42:10 +0900

    Add script to plot peak angle from CSV data

---
+++ src/plotPeakAngle.py
@@ -0,0 +1,25 @@
+# wnaEstimation.pyの中で、作成したdictionary.csvを読み込んで、グラフを作成する
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# constant
+freqLabel = ['3.16 Hz', '5.62 Hz', '10 Hz', '17.8 Hz', '31.6 Hz', '56.2 Hz', '100 Hz',
+             '178 Hz', '316 Hz', '562 Hz', '1 kHz', '1.78 kHz']
+# Read the CSV file
+df = pd.read_csv('../execute/1990-2-11/peakAngleObs.csv')
+
+# Convert 'Epoch' to datetime
+epochAry = pd.to_datetime(df['Epoch'], unit='ns').values
+
+# Plot the graph
+fig, axs = plt.subplots(6, 2, figsize=(10, 6), sharex='col')
+axs = axs.flatten()
+for i in range(len(freqLabel)):
+    axs[i].scatter(epochAry, df['angleAtPeakEpwrCh'+str(i)], marker='*', s=5, label = 'E', color='b')
+    axs[i].scatter(epochAry, df['angleAtPeakBpwrCh'+str(i)], marker='*', s=5, label = 'B', color='r')
+    axs[i].set_xlabel('time [UT]')
+    axs[i].set_ylabel('Angle [deg]\n@'+freqLabel[i])
+    axs[i].set_ylim(0, 180)
+plt.legend(loc='upper right')
+plt.tight_layout()
+plt.show()
