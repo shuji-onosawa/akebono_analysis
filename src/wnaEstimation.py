0e77b8e97224f378af7240d23462c335a94cd7c9
Shuji Onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2023-12-18 14:43:29 +0900

    Update user authentication logic Add function to estimate wave normal angle

--- src/wnaEstimation.py
+++ src/wnaEstimation.py
@@ -5,46 +5,42 @@
 from calc_pwr_matrix_angle_vs_freq import make_wave_mgf_dataset
 import numpy as np
 from utilities import find_zero_cross_idx
-from matplotlib import pyplot as plt
 import csv
 import os
 
-# Input parameters
-date = '1990-02-11'
-startTime = date + 'T18:05:00'
-endTime = date + 'T18:09:00'
 
-# Load data
-ds = make_wave_mgf_dataset(date=date, mca_datatype='pwr')
-subDataset = ds.sel(Epoch=slice(startTime, endTime))
+def split_dataset_into_half_spins(date, start_time, end_time):
+    # Convert input times to datetime format
+    start_time = date + 'T' + start_time
+    end_time = date + 'T' + end_time
 
-angleB0EyArray = subDataset['angle_b0_Ey'].values
-EpwrArray = subDataset['akb_mca_Emax_pwr'].values
+    # Load data
+    ds = make_wave_mgf_dataset(date=date, mca_datatype='pwr')
+    sub_dataset = ds.sel(Epoch=slice(start_time, end_time))
 
-angleB0sByArray = subDataset['angle_b0_sBy'].values
-angleB0BloopArray = subDataset['angle_b0_Bloop'].values
-BpwrArray = subDataset['akb_mca_Bmax_pwr'].values
-Epoch = subDataset.coords['Epoch'].values
+    angleB0EyAry = sub_dataset['angle_b0_Ey'].values
 
-# Find half spin index range for E field
-posToNegIdx, negToPosIdx = find_zero_cross_idx(angleB0EyArray)
-halfSpinIdxRangeList = []
+    # Find half spin index range for E field
+    pos_to_neg_idx, neg_to_pos_idx = find_zero_cross_idx(angleB0EyAry)
+    half_spin_idx_range_list = []
 
-if posToNegIdx[0] > negToPosIdx[0]:
-    for i in range(len(posToNegIdx) - 1):
-        halfSpinIdxRangeList.append([negToPosIdx[i] + 1, posToNegIdx[i] + 1])
-        halfSpinIdxRangeList.append([posToNegIdx[i] + 1, negToPosIdx[i + 1] + 1])
-else:
-    for i in range(len(posToNegIdx)):
-        halfSpinIdxRangeList.append([posToNegIdx[i] + 1, negToPosIdx[i] + 1])
-        halfSpinIdxRangeList.append([negToPosIdx[i] + 1, posToNegIdx[i + 1] + 1])
+    if pos_to_neg_idx[0] > neg_to_pos_idx[0]:
+        for i in range(len(pos_to_neg_idx) - 1):
+            half_spin_idx_range_list.append([neg_to_pos_idx[i] + 1, pos_to_neg_idx[i] + 1])
+            half_spin_idx_range_list.append([pos_to_neg_idx[i] + 1, neg_to_pos_idx[i + 1] + 1])
+    else:
+        for i in range(len(pos_to_neg_idx)):
+            half_spin_idx_range_list.append([pos_to_neg_idx[i] + 1, neg_to_pos_idx[i] + 1])
+            half_spin_idx_range_list.append([neg_to_pos_idx[i] + 1, pos_to_neg_idx[i + 1] + 1])
 
-# split dataset into half spin
-halfSpinDatasetList = []
-for i in range(len(halfSpinIdxRangeList)):
-    halfSpinDatasetList.append(subDataset.sel(Epoch=slice(Epoch[halfSpinIdxRangeList[i][0]],
-                                                          Epoch[halfSpinIdxRangeList[i][1]])))
-epwrArySubDS = halfSpinDatasetList[0]['akb_mca_Emax_pwr'].values
+    # Split dataset into half spin
+    half_spin_dataset_list = []
+    epoch = sub_dataset.coords['Epoch'].values
+    for i in range(len(half_spin_idx_range_list)):
+        half_spin_dataset_list.append(sub_dataset.sel(Epoch=slice(epoch[half_spin_idx_range_list[i][0]],
+                                                                  epoch[half_spin_idx_range_list[i][1]])))
+
+    return half_spin_dataset_list
 
 
 def getAngleAtPeakPwr(angleAry, pwrAry):
@@ -135,7 +131,7 @@ def saveAngleAtPeakPwr(angleAtPeakPwrDict):
     Args:
         angleAtPeakPwrDict (dict): halfSpinごと最初の時刻、各チャンネルの最大値の角度の辞書
     """
-    saveDir = '../execute/'+date+'/'
+    saveDir = '../execute/wnaEstimation/'
     saveName = 'peakAngleObs.csv'
     os.makedirs(saveDir, exist_ok=True)
     with open(saveDir+saveName, 'w') as f:
@@ -146,7 +142,7 @@ def saveAngleAtPeakPwr(angleAtPeakPwrDict):
         writer.writerows(zip(*angleAtPeakPwrDict.values()))
 
 
-def readSimulatedAngleAtPeakPwr(freq, mode):
+def loadSimulatedAngleAtPeakPwrByCh(freq, mode):
     """
     ../execute/pyEmax_Bmax_angle_freq-freq_mode-mode.csvからWNA列と方位角行の(θEmax, θBmax)(理論値)の行列を読み込む
     Args:
@@ -155,7 +151,7 @@ def readSimulatedAngleAtPeakPwr(freq, mode):
     Returns:
         simResultDict (dict): wna行のheader、方位角列のheader、模擬観測値θEmaxの行列 (WNA行, 方位角列)、模擬観測値θBmaxの行列 (WNA行, 方位角列)
     """
-    csvFileName = '../execute/'+date+'/pyEmax_Bmax_angle_freq-'+freq+'_mode-'+mode+'.csv'
+    csvFileName = '../execute/SimulatedObservation/pyEmax_Bmax_angle_freq-'+freq+'_mode-'+mode+'.csv'
 
     data = []
     wnaList = []
@@ -183,40 +179,123 @@ def readSimulatedAngleAtPeakPwr(freq, mode):
             thetaBmaxList.append(thetaBmaxRow)
     # dictionaryに追加
     simResultDict = {}
-    simResultDict['wna'] = np.array(wnaList)
-    simResultDict['azimathAngle'] = np.array(azimathAngleList)
+    simResultDict['wna header'] = np.array(wnaList)
+    simResultDict['azimathAngle header'] = np.array(azimathAngleList)
     simResultDict['thetaEmax'] = np.array(thetaEmaxList)
     simResultDict['thetaBmax'] = np.array(thetaBmaxList)
 
     return simResultDict
 
 
-def wnaEstimation(angleAtPeakPwrDict):
+def wnaEstimationByCh(angleAtPeakPwrDict, ch):
     """
     Args:
-        angleAtPeakPwrDict (dict): halfSpinごと最初の時刻、各チャンネルの最大値の角度の辞書
-
+        angleAtPeakPwrDict (dict): 以下を格納した辞書: halfSpinごと最初の時刻、各チャンネルの最大値の角度
+        ch (int): チャンネル番号
     Returns:
-        wnaDict (dict): halfSpinごと最初の時刻、L mode 仮定時の各チャンネルのWNAの辞書、 R mode 仮定時の各チャンネルのWNAの辞書
+        wnaDictByCh (dict): 以下を格納した辞書: L mode 仮定時のWNA、その配列長に対応する時刻、R mode 仮定時のWNA、その配列長に対応する時刻
     """
-    wnaDict = {}
-    wnaDict['Epoch'] = angleAtPeakPwrDict['Epoch']
-
     freqList = [3.15, 5.62, 10, 17.8,
                 31.6, 56.2, 100, 178,
                 316, 562, 1000, 1780,
                 3160, 5620, 10000, 17800]
+
+    wnaDictByCh = {}
+    timeLmode = []
+    timeRmode = []
+    LmodeWNA = []
+    # LmodeAziAngle = [] 今後実装するかも
+    RmodeWNA = []
+    # RmodeAziAngle = [] 今後実装するかも
+
+    # 観測値を配列に格納
+    EpwrPeakAngle = angleAtPeakPwrDict['angleAtPeakEpwrCh{}'.format(ch)]
+    BpwrPeakAngle = angleAtPeakPwrDict['angleAtPeakBpwrCh{}'.format(ch)]
+    EAngleError = angleAtPeakPwrDict['EangleError']
+    BAngleError = angleAtPeakPwrDict['BangleError']
+    BloopAngleError = angleAtPeakPwrDict['BloopangleError']
+
+    # WNA推定
+    # 模擬観測値の読み込み
+    LmodeSimuResultDict = loadSimulatedAngleAtPeakPwrByCh(str(freqList[ch]), 'l')
+    RmodeSimuResultDict = loadSimulatedAngleAtPeakPwrByCh(str(freqList[ch]), 'r')
+    WNAHeader = LmodeSimuResultDict['wna header']
+    azimuthAngleHeader = LmodeSimuResultDict['azimathAngle header']
+    LmodeThetaEmaxMatrix = LmodeSimuResultDict['thetaEmax']
+    LmodeThetaBmaxMatrix = LmodeSimuResultDict['thetaBmax']
+    RmodeThetaEmaxMatrix = RmodeSimuResultDict['thetaEmax']
+    RmodeThetaBmaxMatrix = RmodeSimuResultDict['thetaBmax']
+
+    # 各スピンごとにWNAを推定する
+    for i in range(len(EpwrPeakAngle)):
+        LmodeWNAbySpin = []
+        RmodeWNAbySpin = []
+
+        obsEpeakAngleLower = EpwrPeakAngle[i] - EAngleError[i]
+        obsEpeakAngleUpper = EpwrPeakAngle[i] + EAngleError[i]
+        if ch < 10:
+            obsBpeakAngleLower = BpwrPeakAngle[i] - BAngleError[i]
+            obsBpeakAngleUpper = BpwrPeakAngle[i] + BAngleError[i]
+        else:
+            obsBpeakAngleLower = BpwrPeakAngle[i] - BloopAngleError[i]
+            obsBpeakAngleUpper = BpwrPeakAngle[i] + BloopAngleError[i]
+        # 模擬観測値のWNA行の値を1つずつ読み込み、観測値の範囲に含まれるかどうかを判定する
+        for j in range(len(WNAHeader)):
+            for k in range(len(azimuthAngleHeader)):
+                # L mode 仮定時のWNAを推定
+                if obsEpeakAngleLower <= LmodeThetaEmaxMatrix[j, k] <= obsEpeakAngleUpper and \
+                        obsBpeakAngleLower <= LmodeThetaBmaxMatrix[j, k] <= obsBpeakAngleUpper:
+                    LmodeWNAbySpin.append(WNAHeader[j])
+                # R mode 仮定時のWNAを推定
+                if obsEpeakAngleLower <= RmodeThetaEmaxMatrix[j, k] <= obsEpeakAngleUpper and \
+                        obsBpeakAngleLower <= RmodeThetaBmaxMatrix[j, k] <= obsBpeakAngleUpper:
+                    RmodeWNAbySpin.append(WNAHeader[j])
+        LmodeWNA+=LmodeWNAbySpin
+        RmodeWNA+=RmodeWNAbySpin
+        # *modeWNAbySpinが空の場合は、time*modeにはなにも追加しない
+        # *modeWNAbySpinが空でない場合は、対応する時刻を*modeWNAbySpinの長さ分、time*modeに追加する
+        if len(LmodeWNAbySpin) != 0:
+            timeLmode += [angleAtPeakPwrDict['Epoch'][i]] * len(LmodeWNAbySpin)
+        if len(RmodeWNAbySpin) != 0:
+            timeRmode += [angleAtPeakPwrDict['Epoch'][i]] * len(RmodeWNAbySpin)
+
+    # dictionaryに追加
+    wnaDictByCh['LmodeWNA'] = np.array(LmodeWNA)
+    wnaDictByCh['timeLmode'] = np.array(timeLmode)
+    wnaDictByCh['RmodeWNA'] = np.array(RmodeWNA)
+    wnaDictByCh['timeRmode'] = np.array(timeRmode)
+
+    return wnaDictByCh
+
+
+def wnaEstimationByChAll(angleAtPeakPwrDict):
+    """
+    Args:
+        angleAtPeakPwrDict (dict): 以下を格納した辞書: halfSpinごと最初の時刻、各チャンネルの最大値の角度
+    Returns:
+        wnaDictByChAll (dict): 以下を格納した辞書: L mode 仮定時のWNA、その配列長に対応する時刻、R mode 仮定時のWNA、その配列長に対応する時刻
+    """
+    wnaDictByChAll = {}
     for ch in range(16):
-        EpwrPeakAngle = angleAtPeakPwrDict['angleAtPeakEpwrCh{}'.format(ch)]
-        BpwrPeakAngle = angleAtPeakPwrDict['angleAtPeakBpwrCh{}'.format(ch)]
-        wnaDict['angleAtPeakEpwrCh{}'.format(ch)] = EpwrPeakAngle
-        wnaDict['angleAtPeakBpwrCh{}'.format(ch)] = BpwrPeakAngle
+        wnaDictByCh = wnaEstimationByCh(angleAtPeakPwrDict, ch)
+        wnaDictByChAll['LmodeWNACh{}'.format(ch)] = wnaDictByCh['LmodeWNA']
+        wnaDictByChAll['timeLmodeCh{}'.format(ch)] = wnaDictByCh['timeLmode']
+        wnaDictByChAll['RmodeWNACh{}'.format(ch)] = wnaDictByCh['RmodeWNA']
+        wnaDictByChAll['timeRmodeCh{}'.format(ch)] = wnaDictByCh['timeRmode']
+    return wnaDictByChAll
 
-        # L mode 仮定時のWNA推定
-        LmodeWNA = np.zeros(len(EpwrPeakAngle))
-        # 模擬観測値の読み込み
-        simResultDict = readSimulatedAngleAtPeakPwr(str(freqList[ch]), 'l')
 
-angleAtPeackPwr = calcAngleAtPeakPwr(halfSpinDatasetList)
-# csvファイルに書き出す
-saveAngleAtPeakPwr(angleAtPeackPwr)
+def saveWNAEstimationByChAll(wnaDictByChAll):
+    """
+    Args:
+        wnaDictByChAll (dict): 以下を格納した辞書: L mode 仮定時のWNA、その配列長に対応する時刻、R mode 仮定時のWNA、その配列長に対応する時刻
+    """
+    saveDir = '../execute/wnaEstimation/'
+    saveName = 'wnaEstimationByChAll.csv'
+    os.makedirs(saveDir, exist_ok=True)
+    with open(saveDir+saveName, 'w') as f:
+        writer = csv.writer(f)
+        # dictionary の key を header として書き出す
+        writer.writerow(wnaDictByChAll.keys())
+        # dictionary の value を書き出す
+        writer.writerows(zip(*wnaDictByChAll.values()))
